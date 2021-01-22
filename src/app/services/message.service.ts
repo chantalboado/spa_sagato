6f258332e2f8a9e294782380db1cdfdb1cf77b4d
chantalboado <chantu.boado@gmail.com>
2021-01-22 14:46:13 -0300

    enviroment setup

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class MessageService {
 constructor(private _http: HttpClient) { }
 sendMessage(body) {
-  const url = `${process.env.API_URL}/formulario`;
+  const url = environment.API_URL;
   return this._http.post(url, body);
  }
 }
